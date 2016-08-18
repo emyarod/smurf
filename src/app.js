487c3c63d4ac5d6b4a5abb06be853f733e02ca2c
emyarod <allllaboutyou@gmail.com>
2016-08-17 19:12:30 -0700

    Use index route

--- src/app.js
+++ src/app.js
@@ -2,45 +2,40 @@
 
 // simple express server
 import fs from 'fs';
+import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import Remarkable from 'remarkable';
-import cheerio from 'cheerio';
-import htmlmin from 'htmlmin';
 import { scrape } from './routes/smurfs';
+import routes from './routes/index';
 
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static(`${__dirname}/public`));
-app.get('/', (req, res) => {
-  res.sendfile('./public/index.html');
-});
+// app.use(express.static(`${__dirname}/public`));
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
 
 // POST gets urlencoded bodies
 app.post('/', urlencodedParser, (req, res) => {
   if (!req.body) return res.sendStatus(400);
   const filePath = `${__dirname}/../status.md`;
   const input = req.body.input;
-  const html = fs.readFileSync(`${__dirname}/public/index.html`, 'utf8');
-  const $ = cheerio.load(html);
 
   fs.writeFile(filePath, input, (err) => {
     if (err) throw err;
     const md = new Remarkable();
 
     scrape().then((value) => {
-      const scriptNode = `
-        <script>
-          const outputDiv = document.getElementById('output');
-          outputDiv.innerHTML = '${htmlmin(md.render(value))}';
-
-          const [table] = document.getElementsByTagName('table');
-          table.className = 'table';
-        </script>`;
-      $('body').append(scriptNode);
-      // res.send(md.render(value));
-      res.send($.html());
+      const marked = md.render(value);
+      if (!marked.indexOf('<table>')) {
+        res.render('index', { output: `<table class="table table-hover">${marked.slice(7)}` });
+      } else {
+        res.render('index', { output: marked });
+      }
     });
   });
 });
